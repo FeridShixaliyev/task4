628c43ba5ab2b0f121b1b3165958577ebabf8bfe
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-10 11:31:18 +0400

    task1

---
+++ addelementtoarray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in Addnumtoarray(arr))
+            {
+            Console.WriteLine(item);
+
+            }
+           
+        }
+
+        static int[] Addnumtoarray(int[] arr,int num)
+        {           
+                       
+            int max = arr.Length - 1;//bu arrayin en sonuncu elementidir
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                num = arr[max+1];
+            }
+            return arr;
+        }
+    }
+}
