1bead0ebf6a12a3571aad63c7a5b329c8d272a5a
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-10 11:31:46 +0400

    task2

---
+++ nospace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Nospace("salam dunya"));
+        }
+        static string Nospace( string str)
+        {
+            char a =  ' ';
+            char b = '';//bu hissesini duzelde bilmedim
+            for(int i = 0; i < str.Length; i++)
+            {
+                if(str[i]==a )
+                {
+                    a = b;
+                }
+            }
+            return str;
+        }
+    }
+}
