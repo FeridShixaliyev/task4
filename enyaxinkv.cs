76d24aa31f6cddc46173d3eb0a60df217f4b2014
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-10 11:32:19 +0400

    task3

---
+++ enyaxinkv.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enyaxinkv(22));
+        }
+       static int Enyaxinkv(int num)
+        {
+            int i = 0;           
+            while (i*i <= num)
+            {
+                i++;
+            }
+            return i-1;
+        }
+    }
+}
